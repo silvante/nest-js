913cd414bff195bed6d2c8acf75eebc956e1ecf7
silvante <khamidov.ko@gmail.com>
2025-04-01 10:58:52 -0400

    delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -36,5 +37,10 @@ export class UsersController {
     @Body() updateUserDto: UpdateUserDto,
   ) {
     return await this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -29,4 +29,8 @@ export class UsersService {
   updateUser(id: number, updateUserDetails: UpdateUserParams) {
     return this.userRepository.update({ id }, { ...updateUserDetails });
   }
+
+  deleteUser(id: number) {
+    return this.userRepository.delete({ id });
+  }
 }
