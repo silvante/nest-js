48935afa07e96148a2310490fc64154e46a00966
silvante <khamidov.ko@gmail.com>
2025-04-12 08:58:40 -0400

    deleting useless cats models

--- src/cats/cats.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatsService } from './cats.service';
-
-@Module({
-  providers: [CatsService]
-})
-export class CatsModule {}
--- src/cats/cats.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class CatsService {
-    getAllCats() : string {
-        return "returns list of cats";
-    }
-}
