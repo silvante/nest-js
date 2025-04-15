765f1debb0d239008fc92172d74431385e85492e
silvante <khamidov.ko@gmail.com>
2025-04-15 12:52:28 -0400

    post

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,6 +6,8 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    console.log(request);
     return true;
   }
 }
