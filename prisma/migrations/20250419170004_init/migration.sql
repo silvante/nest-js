27fcba4df6b488dad05b757b9ea97e250c7c51bb
silvante <khamidov.ko@gmail.com>
2025-04-19 22:02:29 +0500

    new git

--- prisma/migrations/20250419170004_init/migration.sql
+++ prisma/migrations/20250419170004_init/migration.sql
@@ -15,6 +15,10 @@ CREATE TABLE `Post` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `creator_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_creator_id_fkey` FOREIGN KEY (`creator_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
