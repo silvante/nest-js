1b140761e80548abb2517b5f3eca405fe13fc13d
silvante <khamidov.ko@gmail.com>
2025-04-20 21:43:43 +0500

    code .

--- src/mailers/mailer.module.ts
+++ src/mailers/mailer.module.ts
@@ -2,22 +2,25 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 @Module({
   imports: [
     MailerModule.forRoot({
       transport: {
         host: 'smtp.gmail.com',
-        port: 465, // Use 465 for SSL or 587 for TLS
-        secure: true, // Use `true` for SSL
+        port: 465,
+        secure: true,
         auth: {
-          user: process.env.SMTP_EMAIL, // Use env variable
-          pass: process.env.SMTP_PASSWORD, // Use env variable
+          user: process.env.SMTP_EMAIL,
+          pass: process.env.SMTP_PASSWORD,
         },
+      },
+      defaults: {
+        from: '"Nuna" 82mailer@gmail.com',
       },
     }),
   ],
-  exports: [MailerModule], // This is correct
+  exports: [MailerModule],
 })
 export class EmailModule {}
