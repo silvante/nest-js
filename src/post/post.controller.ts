46178ab28cd07a301c7d076484eb8511383862fa
silvante <khamidov.ko@gmail.com>
2025-04-21 14:10:34 +0500

    lessons

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,14 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreateNewPostDTO } from './dtos/createPost.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('post')
 export class PostController {
-  constructor(
-    private postService: PostService,
-  ) {}
+  constructor(private postService: PostService) {}
 
   @Get('all')
+  @UseGuards(AuthGuard)
   async findAll() {
     return this.postService.getAllPosts();
   }
