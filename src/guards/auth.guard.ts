49f8663313fcb2f143af1347c99b4b66d148b175
silvante <khamidov.ko@gmail.com>
2025-04-21 14:18:03 +0500

    custom decatrator

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,13 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(request);
+    console.log('inside authguard');
+    const allowed_roles = this.reflector.get(Roles, context.getHandler());
+    console.log(allowed_roles);
+
     return true;
   }
 }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreateNewPostDTO } from './dtos/createPost.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('post')
 export class PostController {
   constructor(private postService: PostService) {}
 
   @Get('all')
+  @Roles(["admin", "owner"])
   @UseGuards(AuthGuard)
   async findAll() {
     return this.postService.getAllPosts();
