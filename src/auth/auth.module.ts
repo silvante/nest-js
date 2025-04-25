8e5136911936d5a5618b4a361b07e65e0910e207
silvante <khamidov.ko@gmail.com>
2025-04-25 15:12:10 +0500

    using passport js

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { EmailModule } from 'src/mailers/mailer.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'coder-test-np924uhbfrc09837fg',
+      signOptions: { expiresIn: '15m' },
     }),
+    PassportModule,
     EmailModule,
     PrismaModule,
   ],
---
+++ src/strategies/local.strategy.ts
@@ -0,0 +1,12 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super();
+  }
+
+  validate(username: string, email: string) {
+    
+  }
+}
